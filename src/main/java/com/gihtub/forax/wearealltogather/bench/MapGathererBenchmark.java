dfb7bc60eccba71230ae62966420c410b7b552cf
forax <forax@univ-mlv.fr>
2024-01-29 10:14:21 +0100

    try to repeat the same operations

--- src/main/java/com/gihtub/forax/wearealltogather/bench/MapGathererBenchmark.java
+++ src/main/java/com/gihtub/forax/wearealltogather/bench/MapGathererBenchmark.java
@@ -39,7 +39,8 @@ import org.openjdk.jmh.annotations.Warmup;
 @SuppressWarnings("static-method")
 public class MapGathererBenchmark {
 
-	private final List<String> values = IntStream.range(0, 100_000).mapToObj(i -> "item" + i).toList();
+	private final List<Integer> integers = IntStream.range(0, 100_000).boxed().toList();
+	private final List<String> strings = IntStream.range(0, 100_000).mapToObj(i -> "item" + i).toList();
 
 	static <T, R> Gatherer<T, ?, R> map(Function<? super T, ? extends R> mapper) {
 		return Gatherer.of(Gatherer.Integrator.ofGreedy((_, element, downstream) -> {
@@ -60,62 +61,114 @@ public class MapGathererBenchmark {
 		return new MapperGatherer();
 	}
 
-	/*
+  /*
 	@Benchmark
 	public int stream_map_sum() {
-		return values.stream().map(String::length).reduce(0, Integer::sum);
+		return strings.stream().map(String::length).reduce(0, Integer::sum);
 	}
 	@Benchmark
 	public int stream_mapToInt_sum() {
-		return values.stream().mapToInt(String::length).sum();
+		return strings.stream().mapToInt(String::length).sum();
 	}
 	@Benchmark
 	public int gatherer_map_sum() {
-		return values.stream().gather(map(String::length)).reduce(0, Integer::sum);
+		return strings.stream().gather(map(String::length)).reduce(0, Integer::sum);
 	}
 	@Benchmark
 	public int gatherer_mapsublcass_sum() {
-		return values.stream().gather(mapSubclass(String::length)).reduce(0, Integer::sum);
-	}*/
-
-	@Benchmark
-	public boolean stream_map_allMatch() {
-		return values.stream().map(String::length).allMatch(v -> v < 10);
+		return strings.stream().gather(mapSubclass(String::length)).reduce(0, Integer::sum);
 	}
-	@Benchmark
-	public boolean stream_mapToInt_allMatch() {
-		return values.stream().mapToInt(String::length).allMatch(v -> v < 10);
-	}
-	@Benchmark
-	public boolean gatherer_map_allMatch() {
-		return values.stream().gather(map(String::length)).allMatch(v -> v < 10);
-	}
-	@Benchmark
-	public boolean gatherer_mapsublcass_allMatch() {
-		return values.stream().gather(mapSubclass(String::length)).allMatch( v -> v < 10);
-	}
+  */
 
 	/*
 	@Benchmark
+	public boolean stream_map_allMatch() {
+		return strings.stream().map(s -> s.charAt(0) + 1).allMatch(v -> v < 128);
+	}
+	@Benchmark
+	public boolean stream_mapToInt_allMatch() {
+		return strings.stream().mapToInt(s -> s.charAt(0) + 1).allMatch(v -> v < 128);
+	}
+
+	private boolean map_check(int add) {
+		return strings.stream().gather(map(s -> s.charAt(0) + add)).allMatch(v -> v < 128);
+	}
+	private boolean mapSubclass_check(int add) {
+		return strings.stream().gather(mapSubclass(s -> s.charAt(0) + add)).allMatch(v -> v < 128);
+	}
+
+	@Benchmark
+	public boolean gatherer_map_allMatch() {
+		return map_check(1);
+	}
+	@Benchmark
+	public boolean gatherer_mapsublcass_allMatch() {
+		return mapSubclass_check(1);
+	}
+	@Benchmark
+	public boolean gatherer_map__allMatch_3() {
+		return map_check(1) && map_check(2) && map_check(3);
+	}
+	@Benchmark
+	public boolean gatherer_mapsublcass__allMatch_3() {
+		return mapSubclass_check(1) && mapSubclass_check(2) && mapSubclass_check(3);
+	}*/
+
+
+	@Benchmark
+	public boolean map_allMatch_repeat_3() {
+		return integers.stream()
+				.map(v -> v + 1)
+				.map(v -> v + 2)
+				.map(v -> v + 3)
+				.allMatch(v -> v < 1_000_000);
+	}
+	@Benchmark
+	public boolean mapToInt_allMatch_repeat_3() {
+		return integers.stream()
+				.mapToInt(v -> v + 1)
+				.map(v -> v + 2)
+				.map(v -> v + 3)
+				.allMatch(v -> v < 1_000_000);
+	}
+	@Benchmark
+	public boolean gatherer_map_allMatch_repeat_3() {
+		return integers.stream()
+				.gather(map(v -> v + 1))
+				.gather(map(v -> v + 2))
+				.gather(map(v -> v + 3))
+				.allMatch(v -> v < 1_000_000);
+	}
+	@Benchmark
+	public boolean gatherer_mapsublcass_allMatch_repeat_3() {
+		return integers.stream()
+				.gather(mapSubclass(v -> v + 1))
+				.gather(mapSubclass(v -> v + 2))
+				.gather(mapSubclass(v -> v + 3))
+				.allMatch(v -> v < 1_000_000);
+	}
+
+  /*
+	@Benchmark
 	public long stream_map_count() {
-		return values.stream().map(String::length).count();
+		return strings.stream().map(String::length).count();
 	}
 	@Benchmark
 	public long stream_mapToInt_count() {
-		return values.stream().mapToInt(String::length).count();
+		return strings.stream().mapToInt(String::length).count();
 	}
 	@Benchmark
 	public long gatherer_map_count() {
-		return values.stream().gather(map(String::length)).count();
+		return strings.stream().gather(map(String::length)).count();
 	}
 
 	@Benchmark
 	public List<Integer> stream_map_toList() {
-		return values.stream().map(String::length).toList();
+		return strings.stream().map(String::length).toList();
 	}
 	@Benchmark
 	public List<Integer> gatherer_map_toList() {
-		return values.stream().gather(map(String::length)).toList();
+		return strings.stream().gather(map(String::length)).toList();
 	}
-	*/
+  */
 }
